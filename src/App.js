06cdccecc7a9b3888d90dcf12c940d1691c9a0c7
hyunki <hyunki08@gmail.com>
2022-09-23 12:46:42 +0900

    라우트 구현 및 기본 레이아웃 디자인 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 
 import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
-  return <DefaultLayout>Hello</DefaultLayout>;
+  return (
+    <Router>
+      <DefaultLayout>
+        <Routes>
+          <Route path="/" element={<div />} />
+          <Route path="/hourly" element={<div />} />
+          <Route path="/daily" element={<div />} />
+          <Route path="/monthly" element={<div />} />
+          <Route path="/yearly" element={<div />} />
+          <Route path="/*" element={<div>not found</div>} />
+        </Routes>
+      </DefaultLayout>
+    </Router>
+  );
 }
 
 export default App;
--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,21 +1,42 @@
-import React, { Children } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Layout, Menu } from "antd";
 import styles from "../styles/DefaultLayout.module.css";
 
 const { Header, Footer, Content, Sider } = Layout;
 
 const items = [
-  { key: "dashboard", label: "Dashboard" },
-  { key: "hourly", label: "hourly" },
-  { key: "daily", label: "Daily" },
-  { key: "monthly", label: "Monthly" },
-  { key: "yearly", label: "Yearly" },
+  { key: "/", label: "Dashboard" },
+  { key: "/hourly", label: "Hourly" },
+  { key: "/daily", label: "Daily" },
+  { key: "/monthly", label: "Monthly" },
+  { key: "/yearly", label: "Yearly" },
 ];
 
 const DefaultLayout = ({ children }) => {
+  const [headerTitle, setHeaderTitle] = useState("");
+  const [selectedKey, setSelectedKey] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onClickMenu = ({ key }) => {
+    navigate(key);
+    setSelected(key);
+  };
+
+  const setSelected = (key) => {
+    setSelectedKey(key);
+    setHeaderTitle(items.find((item) => item.key === key)?.label);
+  };
+
+  useEffect(() => {
+    setSelected(location.pathname);
+  }, [location]);
+
   return (
     <Layout hasSider>
       <Sider
+        theme="light"
         style={{
           overflow: "auto",
           height: "100vh",
@@ -27,10 +48,11 @@ const DefaultLayout = ({ children }) => {
       >
         <div className={styles.logo}>Power Demand App</div>
         <Menu
-          theme="dark"
+          theme="light"
           mode="inline"
-          defaultSelectedKeys={["4"]}
+          selectedKeys={[selectedKey]}
           items={items}
+          onClick={onClickMenu}
         />
       </Sider>
       <Layout
@@ -39,12 +61,7 @@ const DefaultLayout = ({ children }) => {
           marginLeft: 200,
         }}
       >
-        <Header
-          className={styles.siteLayoutBackground}
-          style={{
-            padding: 0,
-          }}
-        />
+        <Header className={styles.siteLayoutHeader}>{headerTitle}</Header>
         <Content
           style={{
             margin: "24px 16px 0",
@@ -66,7 +83,7 @@ const DefaultLayout = ({ children }) => {
             textAlign: "center",
           }}
         >
-          power-demand-app ©2018 Created by hyunki
+          power demand app ©2022 Created by hyunki
         </Footer>
       </Layout>
     </Layout>
