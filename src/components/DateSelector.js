70ab46445b438ff3601f9a83111a3d952208e0dd
hyunki <hyunki08@gmail.com>
2022-11-27 15:59:49 +0900

    날짜 선택 부분 클리어 버튼 제거

--- src/components/DateSelector.js
+++ src/components/DateSelector.js
@@ -6,62 +6,63 @@ import { CloseOutlined } from "@ant-design/icons";
 import { DateContext } from "../contexts/dateContext";
 
 const DateSelector = ({ fethchData, data, setData, picker = "" }) => {
-  const meta = useContext(DateContext);
-  const [dates, setDates] = useState([]);
+    const meta = useContext(DateContext);
+    const [dates, setDates] = useState([]);
 
-  const disabledDate = (current) => {
-    return !current.isBetween(meta.minDate, meta.maxDate);
-  };
+    const disabledDate = (current) => {
+        return !current.isBetween(meta.minDate, meta.maxDate);
+    };
 
-  const onClickDeleteDate = (date) => {
-    const index = dates.findIndex((d) => d === date);
-    setData({
-      ...data,
-      datasets: data.datasets.filter((d) => d.label !== date),
-    });
-    setDates([...dates.slice(0, index), ...dates.slice(index + 1)]);
-  };
+    const onClickDeleteDate = (date) => {
+        const index = dates.findIndex((d) => d === date);
+        setData({
+            ...data,
+            datasets: data.datasets.filter((d) => d.label !== date),
+        });
+        setDates([...dates.slice(0, index), ...dates.slice(index + 1)]);
+    };
 
-  const onClickAddDate = (date) => {
-    setDates([date, ...dates]);
-    fethchData(date);
-  };
+    const onClickAddDate = (date) => {
+        setDates([date, ...dates]);
+        fethchData(date);
+    };
 
-  const onClickClearDates = () => {
-    setData({ ...data, datasets: [] });
-    setDates([]);
-  };
+    const onClickClearDates = () => {
+        setData({ ...data, datasets: [] });
+        setDates([]);
+    };
 
-  return (
-    <div className={styles.datepickerwrapper}>
-      <DatePicker
-        className={styles.datepicker}
-        defaultValue={moment(meta.maxDate)}
-        disabledDate={disabledDate}
-        onChange={(_, date) => onClickAddDate(date)}
-        picker={picker}
-      />
-      <div className={styles.dates}>
-        {!!dates && dates.length > 0 && (
-          <Button type="primary" onClick={onClickClearDates}>
-            Clear
-          </Button>
-        )}
-        {!!dates &&
-          dates.length > 0 &&
-          dates.map((date, i) => (
-            <div
-              key={i}
-              className={styles.date}
-              onClick={() => onClickDeleteDate(date)}
-            >
-              {date}
-              <CloseOutlined className={styles.datedelete} />
+    return (
+        <div className={styles.datepickerwrapper}>
+            <DatePicker
+                className={styles.datepicker}
+                defaultValue={moment(meta.maxDate)}
+                disabledDate={disabledDate}
+                onChange={(_, date) => onClickAddDate(date)}
+                picker={picker}
+                allowClear={false}
+            />
+            <div className={styles.dates}>
+                {!!dates && dates.length > 0 && (
+                    <Button type="primary" onClick={onClickClearDates}>
+                        Clear
+                    </Button>
+                )}
+                {!!dates &&
+                    dates.length > 0 &&
+                    dates.map((date, i) => (
+                        <div
+                            key={i}
+                            className={styles.date}
+                            onClick={() => onClickDeleteDate(date)}
+                        >
+                            {date}
+                            <CloseOutlined className={styles.datedelete} />
+                        </div>
+                    ))}
             </div>
-          ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default DateSelector;
